4e2e49074caab03bf12e59ec8ce13b3b228d4136
Rafael Moraes Hiesch <rafaelmh@labcmi.org.br>
2023-04-24 15:14:04 -0300

    Iniciando os estudos

---
+++ main.py
@@ -0,0 +1,10 @@
+# Passo 1 - Percorrer os arquivis da base de dados (Pasta Vendas)
+import os
+
+lista_arquivo = os.listdir(r"C:\Users\rafaelmh\Documents\Estudos\Python Hashtag\Vendas")
+print(lista_arquivo)
+# Passo 2 - Importar as bases de dados de vendas
+
+# Passo 3 - Tratar / Compilar as bases de dados
+
+# Passo 4 - Calcular o produto mais vendido (em quantidade)
