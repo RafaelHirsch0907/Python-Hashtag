3478b90fbbcf0b9bb813277c96fe5c56775853be
Rafael Moraes Hiesch <rafaelmh@labcmi.org.br>
2023-04-27 11:28:07 -0300

    Finalizado

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import pandas as pd
 import plotly.express as px
 
-listaArquivo = os.listdir("C:\Estudos\Python Hashtag\Vendas")
+listaArquivo = os.listdir("C:\Estudos\Python-Hashtag\Vendas")
 #print(listaArquivo)
 
 # Passo 2 - Importar as bases de dados de vendas
@@ -12,7 +12,7 @@ tabelaTotal = pd.DataFrame()
 for arquivo in listaArquivo:
     if "Vendas" in arquivo:
         # importar o arquivo
-        tabela = pd.read_csv(f"C:\Estudos\Python Hashtag\Vendas\{arquivo}")
+        tabela = pd.read_csv(f"C:\Estudos\Python-Hashtag\Vendas\{arquivo}")
         tabelaTotal = pd.concat([tabelaTotal, tabela])
 
 # Passo 3 - Tratar / Compilar as bases de dados
@@ -33,7 +33,7 @@ tabelaFaturamento = tabelaFaturamento[["Faturamento"]].sort_values(by="Faturamen
 # Passo 6 - Calcular a loja/cidade que mais vendeu (em faturamento) - criar um gráfico/dashboard
 tabelaLojas = tabelaTotal.groupby('Loja').sum()
 tabelaLojas = tabelaLojas[['Faturamento']] 
-print(tabelaLojas)
+#print(tabelaLojas)
 
 grafico = px.bar(tabelaLojas,x=tabelaLojas.index,y='Faturamento')
 grafico.show()
